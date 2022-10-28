85d64294ee96ab2cec5e7aadb693ac4150fbf2ba
ittehad-abdullah <ittehad15-3530@diu.edu.bd>
2022-10-28 22:38:07 +0600

    details added

--- src/Share/Details/Details.js
+++ src/Share/Details/Details.js
@@ -8,13 +8,13 @@ import ReactToPrint from 'react-to-print';
 const Details = () => {
   const course = useLoaderData();
   const { title, details, image_url, category_id } = course;
- const ref =useRef()
+  const ref = useRef()
 
   return (
     <div>
       {/* <ReactToPrint trigger={()=><button className='download-btn'>Print</button>} content={() => ref.current} /> */}
 
-     
+
       <Card ref={ref}>
         <Card.Img variant="top" src={image_url} />
         <Card.Body>
@@ -23,12 +23,12 @@ const Details = () => {
             {details}
           </Card.Text>
           <div className='but-container'>
-          <ReactToPrint trigger={()=><button className='download-btn'>Print PDF</button>} content={() => ref.current} />
-          <Link to='/premium'>
-            <div className='text-end p-5 fw-bold'>
-              <Button variant="warning">GET PREMIUM</Button>
-            </div>
-          </Link>
+            <ReactToPrint trigger={() => <button className='download-btn'>Print PDF</button>} content={() => ref.current} />
+            <Link to='/premium'>
+              <div className='text-end p-5 fw-bold'>
+                <Button variant="warning">GET PREMIUM</Button>
+              </div>
+            </Link>
           </div>
           <Link to={`/category/${category_id}`}>
             <Button className='btn w-100' variant="secondary">
@@ -42,3 +42,36 @@ const Details = () => {
 };
 
 export default Details;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
